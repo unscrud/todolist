e48ea056e68675fb9c36042ea8928b2c8cc3db7b
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-14 17:01:42 -0300

    Reduzindo código com lombok

--- src/main/java/dev/unscrud/todolist/user/User.java
+++ src/main/java/dev/unscrud/todolist/user/User.java
@@ -1,31 +1,10 @@
 package dev.unscrud.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class User {
     private String username;
     private String name;
     private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
