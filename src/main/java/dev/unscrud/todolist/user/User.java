44a7d209f23f3e3e505bc795908b27f297553a9e
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-14 20:26:28 -0300

    adição do id e do created at

--- src/main/java/dev/unscrud/todolist/user/User.java
+++ src/main/java/dev/unscrud/todolist/user/User.java
@@ -1,10 +1,26 @@
 package dev.unscrud.todolist.user;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users")
 public class User {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private String uuid;
+
     private String username;
     private String name;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
