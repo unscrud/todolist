64cc599d04b32fe4ac6f485f9f9797337c923596
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-14 16:58:39 -0300

    modelo do usuário

---
+++ src/main/java/dev/unscrud/todolist/user/User.java
@@ -0,0 +1,31 @@
+package dev.unscrud.todolist.user;
+
+public class User {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
