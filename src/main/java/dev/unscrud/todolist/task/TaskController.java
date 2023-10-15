050b4e72b5e9dfffe5f82cea775c9003c0c687a4
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-15 15:09:50 -0300

    criação de tarefa

--- src/main/java/dev/unscrud/todolist/task/TaskController.java
+++ src/main/java/dev/unscrud/todolist/task/TaskController.java
@@ -1,10 +1,20 @@
 package dev.unscrud.todolist.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping
+    public void create(@RequestBody Task task){
+        this.taskRepository.save(task);
+    }
 }
