6b424d98c3e09512cec7bd32a971a666d1726780
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-15 12:15:39 -0300

    estratégia de geração de id e username único

--- src/main/java/dev/unscrud/todolist/user/User.java
+++ src/main/java/dev/unscrud/todolist/user/User.java
@@ -4,8 +4,10 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -14,10 +16,12 @@ import lombok.Data;
 public class User {
 
     @Id
-    @GeneratedValue(generator = "UUID")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String uuid;
 
+    @Column(unique = true)
     private String username;
+    
     private String name;
     private String password;
 
