cbf37e3e1931c03182a77540ee4ba24da522d637
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-15 15:35:00 -0300

    troca do filter pelo oncePerRequestFilter

--- src/main/java/dev/unscrud/todolist/filter/FilterTaskAuth.java
+++ src/main/java/dev/unscrud/todolist/filter/FilterTaskAuth.java
@@ -3,21 +3,21 @@ package dev.unscrud.todolist.filter;
 import java.io.IOException;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        System.out.println("Chegou no filtro ");
-        chain.doFilter(request,response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
     }
 
 }
