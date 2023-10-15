ef9ef4541a78bea4d11ceace1c5b78acc1223433
Daniel Carvalho <daniel.devop@gmail.com>
2023-10-15 12:16:48 -0300

    interface de acesso a dados do usuário

---
+++ src/main/java/dev/unscrud/todolist/user/IUserRepository.java
@@ -0,0 +1,6 @@
+package dev.unscrud.todolist.user;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<User, UUID> {}
