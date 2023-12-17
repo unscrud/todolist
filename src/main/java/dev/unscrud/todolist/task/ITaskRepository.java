d677992e131e124266945b9055027ebb55cc9769
Daniel Carvalho <daniel.devop@gmail.com>
2023-12-17 13:35:03 -0300

    adição da listagem de tarefas por usuário

--- src/main/java/dev/unscrud/todolist/task/ITaskRepository.java
+++ src/main/java/dev/unscrud/todolist/task/ITaskRepository.java
@@ -3,7 +3,9 @@ package dev.unscrud.todolist.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface ITaskRepository extends JpaRepository<Task, UUID> {
-    
+    List<Task> findByIdUser(UUID idUser);
 }
