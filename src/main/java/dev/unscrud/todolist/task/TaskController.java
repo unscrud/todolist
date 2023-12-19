f114febcb606c18d8ea40c4e85ea9c00ca3a90d7
Daniel Carvalho <daniel.devop@gmail.com>
2023-12-19 09:42:18 -0300

    Pegando o id do usuário

--- src/main/java/dev/unscrud/todolist/task/TaskController.java
+++ src/main/java/dev/unscrud/todolist/task/TaskController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
@@ -14,7 +16,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping
-    public void create(@RequestBody Task task){
-        this.taskRepository.save(task);
+    public void create(@RequestBody Task task, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        System.out.println("Chegou no controller "+idUser);
+        //this.taskRepository.save(task);
     }
 }
