304ce750f0e66a7e846340d3d5431a9174805b1a
Daniel Carvalho <daniel.devop@gmail.com>
2023-12-19 11:28:43 -0300

    create retorna um ResponseEntity

--- src/main/java/dev/unscrud/todolist/task/TaskController.java
+++ src/main/java/dev/unscrud/todolist/task/TaskController.java
@@ -1,6 +1,11 @@
 package dev.unscrud.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +21,11 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping
-    public void create(@RequestBody Task task, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody Task task, HttpServletRequest request){
         var idUser = request.getAttribute("idUser");
-        System.out.println("Chegou no controller "+idUser);
+        task.setIdUser((UUID) idUser);
+
         //this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(task);
     }
 }
